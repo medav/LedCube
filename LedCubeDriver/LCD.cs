c006e698bddec56b59ff147a03cb44885629e5f3
Michael Davies <michaelstoby@mchsi.com>
2015-05-24 20:56:29 -0700

    Removed unused assemblies

--- LedCubeDriver/LCD.cs
+++ LedCubeDriver/LCD.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO.Ports;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
 
 namespace LedCube
 {
